d79cd288842c59a15221b6910e4301d820788d58
Godarshan2002 <godarshanreddyenamala@gmail.com>
2025-05-01 11:48:28 +0530

    changed path configuration

--- src/shared/paths/APIPaths.ts
+++ src/shared/paths/APIPaths.ts
@@ -20,5 +20,5 @@ export enum API_PATHS{
 
     GET_MODULES_BY_COURSE_ID="/api/teacher/course/:course_id/getModules",
     GET_MODULE_BYID="/api/teacher/course/module/:module_id/getModule",
-
+    ADD_QUIZ="/api/teacher/module/:module_id/addQuiz",
 }
